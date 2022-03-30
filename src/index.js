a4e74c96b2993cf79859657933e1ab7c18fb567b
Miguel Padrino <me@miguelpadrino.com>
2022-03-30 17:34:49 -0400

    WIP

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/test");
+}
