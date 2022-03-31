28af514cbbdf38697a2ea31f8de2df01170c2077
Miguel Padrino <me@miguelpadrino.com>
2022-03-31 01:26:20 -0400

    WIP

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,19 @@
-const express = require('express');
-const adopterRouter = require('./adopter-route')
-const clinicHistoryRouter = require('./clinicHistory-route')
-const animalRouter = require('./animal-route')
-const supervisorRouter = require('./supervisor-route')
-const workerRouter = require('./worker-route')
+const express = require("express");
+const adopterRouter = require("./adopter-route");
+const clinicHistoryRouter = require("./clinicHistory-route");
+const animalRouter = require("./animal-route");
+const supervisorRouter = require("./supervisor-route");
+const workerRouter = require("./worker-route");
 
-function routerApi(app){
-
+function routerApi(app) {
   const router = express.Router();
-  app.use('/api/v1',router);
+  app.use("/api/v1", router);
 
-  router.use('/adopterRouter',adopterRouter);
-  router.use('/clinicHistoryRouter',clinicHistoryRouter);
-  router.use('/animalRouter',animalRouter);
-  router.use('/supervisorRouter',supervisorRouter);
-  router.use('/workerRouter',workerRouter);
-  
+  router.use("/", adopterRouter);
+  router.use("/", clinicHistoryRouter);
+  router.use("/", animalRouter);
+  router.use("/", supervisorRouter);
+  router.use("/", workerRouter);
 }
 
-module.exports=routerApi;
+module.exports = routerApi;
