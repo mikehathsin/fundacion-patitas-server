b4272493d7404665a0cbc29556d7567b25c0112f
Roner <roner.ortega@gmail.com>
2022-03-30 22:22:46 -0500

    Agregando mas servicios

---
+++ src/services/rescuer-service.js
@@ -0,0 +1,36 @@
+const { Rescuer } = require("../models/rescuer-model");
+
+class RescuerService {
+  static async create({ name }) {
+    if (!name) {
+      throw new Error("the 'name' field is required");
+    }
+
+    const rescuer = new Rescuer({ name });
+    const response = await rescuer.save();
+    return response;
+  }
+
+  static async read() {
+    const response = await Rescuer.find();
+    return response;
+  }
+
+  static async update({ _id, ...data }) {
+    const filter = { _id };
+
+    await Rescuer.findOneAndUpdate(filter, data);
+    const response = await Rescuer.findOne(filter);
+    return response;
+  }
+
+  static async delete({ _id }) {
+    const filter = { _id };
+    const response = await Rescuer.deleteOne(filter);
+    return response;
+  }
+}
+
+module.exports = {
+  RescuerService,
+};
